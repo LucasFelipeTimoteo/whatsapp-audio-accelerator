66b07a772809bef7f8831bbafb32f9cc24761c65
Lucas Felipe <conhecimento54321@gmail.com>
2020-04-17 19:57:14 -0300

    First version

---
+++ content.js
@@ -0,0 +1,36 @@
+const interval = setInterval(() => {
+    const header = document.querySelector("._3auIg")
+    if (header) {
+        console.log(header)
+        clearInterval(interval)
+
+        const button = document.createElement('button')
+        button.innerHTML = "2x"
+
+        button.classList.add('two-times-button')
+
+        button.addEventListener('click', () => {
+            button.classList.toggle('active-button')
+
+            const audios = document.querySelectorAll('audio')
+
+            if (button.classList.contains('active-button')) {
+                audios.forEach(audio => {
+                    audio.playbackRate = 2
+                    audio.defaultPlaybackRate = 2
+                })
+
+            } else {
+                audios.forEach(audio => {
+                    audio.playbackRate = 1
+                    audio.defaultPlaybackRate = 1
+                })
+
+            }
+
+        })
+
+        header.appendChild(button)
+
+    }
+}, 1000)
